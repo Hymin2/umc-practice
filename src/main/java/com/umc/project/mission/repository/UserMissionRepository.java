0768939f1f68dc454ac23e770192495b479bc3ca
khm8246 <khm8246@gmail.com>
2023-12-01 16:28:49 +0900

    feat: add user mission repository

---
+++ src/main/java/com/umc/project/mission/repository/UserMissionRepository.java
@@ -0,0 +1,8 @@
+package com.umc.project.mission.repository;
+
+import com.umc.project.mission.entity.UserMission;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserMissionRepository extends JpaRepository<UserMission, Long> {
+    boolean existsByUser_IdAndMission_Id(Long userId, Long missionId);
+}
