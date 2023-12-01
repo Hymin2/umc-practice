1c9db3aca4619dfa4f44995a0277f05bc4130720
khm8246 <khm8246@gmail.com>
2023-12-01 16:30:06 +0900

    feat: add mission dto

---
+++ src/main/java/com/umc/project/mission/dto/MissionRequestDTO.java
@@ -0,0 +1,27 @@
+package com.umc.project.mission.dto;
+
+import com.umc.project.mission.validation.annotation.ExistMission;
+import com.umc.project.mission.validation.annotation.NotExistUserMission;
+import com.umc.project.restaurant.validation.annotation.ExistRestaurant;
+import com.umc.project.user.validation.annotation.ExistUser;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class MissionRequestDTO {
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @NotExistUserMission
+    public static class UserMissionCreateDTO {
+        @ExistUser
+        private Long userId;
+        @ExistMission
+        private Long missionId;
+    }
+
+
+}
