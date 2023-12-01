16bb328c554c17e02f62d9814e5b7ac84aeaae18
khm8246 <khm8246@gmail.com>
2023-12-01 16:31:17 +0900

    feat: add mission not found error status

--- src/main/java/com/umc/project/global/payload/code/status/ErrorStatus.java
+++ src/main/java/com/umc/project/global/payload/code/status/ErrorStatus.java
@@ -15,7 +15,8 @@ public enum ErrorStatus implements BaseErrorCode {
     _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
 
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "COMMON400", "사용자를 찾을 수 없습니다."),
-    RESTAURANT_NOT_FOUND(HttpStatus.NOT_FOUND, "COMMON400", "식당을 찾을 수 없습니다.");
+    RESTAURANT_NOT_FOUND(HttpStatus.NOT_FOUND, "COMMON400", "식당을 찾을 수 없습니다."),
+    MISSION_NOT_FOUND(HttpStatus.NOT_FOUND, "COMMON400", "미션을 찾을 수 없습니다.");
 
 
     private final HttpStatus httpStatus;
