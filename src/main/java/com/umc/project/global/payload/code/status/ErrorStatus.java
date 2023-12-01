95f0957cea6fde962c6c1f9d67973f9b15f21c4a
khm8246 <khm8246@gmail.com>
2023-12-01 18:03:41 +0900

    feat: add address not found error status

--- src/main/java/com/umc/project/global/payload/code/status/ErrorStatus.java
+++ src/main/java/com/umc/project/global/payload/code/status/ErrorStatus.java
@@ -16,7 +16,8 @@ public enum ErrorStatus implements BaseErrorCode {
 
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "COMMON400", "사용자를 찾을 수 없습니다."),
     RESTAURANT_NOT_FOUND(HttpStatus.NOT_FOUND, "COMMON400", "식당을 찾을 수 없습니다."),
-    MISSION_NOT_FOUND(HttpStatus.NOT_FOUND, "COMMON400", "미션을 찾을 수 없습니다.");
+    MISSION_NOT_FOUND(HttpStatus.NOT_FOUND, "COMMON400", "미션을 찾을 수 없습니다."),
+    ADDRESS_NOT_FOUND(HttpStatus.NOT_FOUND, "COMMON400", "주소를 찾을 수 없습니다.");
 
 
     private final HttpStatus httpStatus;
