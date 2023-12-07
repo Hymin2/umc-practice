18177ee1b7045e14f423b01bee60a5ce25a65ca3
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-12-08 01:41:02 +0900

    .

--- src/main/java/com/umc/project/review/controller/ReviewController.java
+++ src/main/java/com/umc/project/review/controller/ReviewController.java
@@ -1,21 +1,32 @@
 package com.umc.project.review.controller;
 
+import com.umc.project.global.page.validation.annotation.CheckPage;
 import com.umc.project.global.payload.ApiResponse;
+import com.umc.project.global.page.dto.PageDTO;
 import com.umc.project.review.dto.ReviewRequestDTO;
 import com.umc.project.review.dto.ReviewResponseDTO;
 import com.umc.project.review.service.ReviewService;
+import com.umc.project.user.validation.annotation.ExistUser;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/review")
 public class ReviewController {
     private final ReviewService reviewService;
+    @GetMapping
+    public ApiResponse<PageDTO<ReviewResponseDTO.MyReviewDTO>> myReview(@RequestParam @ExistUser Long userId, @RequestParam @CheckPage Integer page){
+        return ApiResponse.onSuccess(reviewService.myReview(userId, page));
+    }
 
     @PostMapping()
     public ApiResponse<ReviewResponseDTO.ReviewCreateDTO> createReview(@RequestBody @Valid ReviewRequestDTO.ReviewCreateDTO reviewCreateDTO){
