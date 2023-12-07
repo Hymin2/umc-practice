867a08d53d375956594a3cded4aab34030223140
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-12-08 01:31:22 +0900

    feat: add myReviewDTO mapper

--- src/main/java/com/umc/project/review/mapper/ReviewMapper.java
+++ src/main/java/com/umc/project/review/mapper/ReviewMapper.java
@@ -1,8 +1,12 @@
 package com.umc.project.review.mapper;
 
+import com.umc.project.global.page.dto.PageDTO;
 import com.umc.project.review.dto.ReviewRequestDTO;
 import com.umc.project.review.dto.ReviewResponseDTO;
+import com.umc.project.review.dto.ReviewResponseDTO.MyReviewDTO;
 import com.umc.project.review.entity.Review;
+import java.util.List;
+import org.springframework.data.domain.Page;
 
 public class ReviewMapper {
     public static Review toReview(ReviewRequestDTO.ReviewCreateDTO reviewCreateDTO){
@@ -15,6 +19,31 @@ public class ReviewMapper {
     public static ReviewResponseDTO.ReviewCreateDTO toReviewCreateDTO(Review review){
         return ReviewResponseDTO.ReviewCreateDTO.builder()
                 .reviewId(review.getId())
+                .build();
+    }
+
+    public static ReviewResponseDTO.MyReviewDTO toMyReviewDTO(Review review){
+        return MyReviewDTO.builder()
+                .ownerNickname(review.getUser().getName())
+                .createdAt(review.getCreatedTime())
+                .starPoint(review.getStartPoint())
+                .content(review.getContent())
+                .build();
+    }
+
+    public static PageDTO<ReviewResponseDTO.MyReviewDTO> toMyReviewPageDTO(Page<Review> review){
+        List<MyReviewDTO> myReviewDTOs = review
+                .stream()
+                .map(ReviewMapper::toMyReviewDTO)
+                .toList();
+
+        return PageDTO.<ReviewResponseDTO.MyReviewDTO>builder()
+                .isLast(review.isLast())
+                .isFirst(review.isFirst())
+                .totalPage(review.getTotalPages())
+                .listSize(review.getSize())
+                .totalElements(review.getTotalElements())
+                .results(myReviewDTOs)
                 .build();
     }
 }
