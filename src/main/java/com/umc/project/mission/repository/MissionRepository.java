ab81c470df40f3ff5c2765da56feb6636d1d0c7b
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-12-08 07:24:01 +0900

    feat: add my mission jpa

--- src/main/java/com/umc/project/mission/repository/MissionRepository.java
+++ src/main/java/com/umc/project/mission/repository/MissionRepository.java
@@ -1,13 +1,15 @@
 package com.umc.project.mission.repository;
 
 import com.umc.project.mission.entity.Mission;
-import com.umc.project.restaurant.entity.Restaurant;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MissionRepository extends JpaRepository<Mission, Long> {
-    Page<Mission> findAllByRestaurantId(Long restaurantId, PageRequest pageRequest);
+    Page<Mission> findAllByRestaurantId(Long restaurantId, Pageable pageRequest);
+    @Query(value = "select m from Mission m inner join UserMission um on um.mission = m where um.user.id =:userId and um.completeStatus =:status")
+    Page<Mission> findAllByUserIdAndStatus(Long userId, String status, Pageable pageRequest);
 }
