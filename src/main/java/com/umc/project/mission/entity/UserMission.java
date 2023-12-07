29bbda64a46a6bdce67b617a25726a7770b022d6
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-12-08 07:24:54 +0900

    feat: add user mission table's index

--- src/main/java/com/umc/project/mission/entity/UserMission.java
+++ src/main/java/com/umc/project/mission/entity/UserMission.java
@@ -8,6 +8,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
@@ -15,12 +16,15 @@ import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 import java.util.Date;
-
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 
 @Entity
-@Table(name = "user_mission_table")
+@Table(name = "user_mission_table", indexes = @Index(name = "user_mission_index", columnList = "user_id, complete_status"))
 @Builder
 @Getter
 @AllArgsConstructor
