211ff82bfac946071649fd865f5e994e72c12638
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-12-08 03:13:45 +0900

    feat: add page dto static factory method

--- src/main/java/com/umc/project/global/page/dto/PageDTO.java
+++ src/main/java/com/umc/project/global/page/dto/PageDTO.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
 
 @Builder
 @Getter
@@ -18,4 +19,15 @@ public class PageDTO<T> {
     Long totalElements;
     Boolean isFirst;
     Boolean isLast;
+
+    public static <T> PageDTO<T> of(Page page, List<T> results){
+        return PageDTO.<T>builder()
+                .isFirst(page.isFirst())
+                .isLast(page.isLast())
+                .listSize(page.getSize())
+                .totalElements(page.getTotalElements())
+                .totalPage(page.getTotalPages())
+                .results(results)
+                .build();
+    }
 }
