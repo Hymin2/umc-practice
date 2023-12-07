de7d3b885ed386801f5d6b296b339aa374f90773
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-12-08 07:26:15 +0900

    refactor: modify my review api parameter to Pageable object

--- src/main/java/com/umc/project/review/controller/ReviewController.java
+++ src/main/java/com/umc/project/review/controller/ReviewController.java
@@ -1,14 +1,16 @@
 package com.umc.project.review.controller;
 
+import com.umc.project.global.page.dto.PageDTO;
 import com.umc.project.global.page.validation.annotation.CheckPage;
 import com.umc.project.global.payload.ApiResponse;
-import com.umc.project.global.page.dto.PageDTO;
 import com.umc.project.review.dto.ReviewRequestDTO;
 import com.umc.project.review.dto.ReviewResponseDTO;
 import com.umc.project.review.service.ReviewService;
 import com.umc.project.user.validation.annotation.ExistUser;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,7 +26,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReviewController {
     private final ReviewService reviewService;
     @GetMapping
-    public ApiResponse<PageDTO<ReviewResponseDTO.MyReviewDTO>> myReview(@RequestParam @ExistUser Long userId, @RequestParam @CheckPage Integer page){
+    public ApiResponse<PageDTO<ReviewResponseDTO.MyReviewDTO>> myReview(@RequestParam @ExistUser Long userId,
+                                                                        @CheckPage @PageableDefault(page = 0, size = 10) Pageable page){
+        System.out.println(page.getPageSize());
         return ApiResponse.onSuccess(reviewService.myReview(userId, page));
     }
 
