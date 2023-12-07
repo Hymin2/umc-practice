06fd6b789d59deca436f6b4fc6b1ce659260a3d3
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2023-12-08 03:14:33 +0900

    feat: add restaurant mission api

--- src/main/java/com/umc/project/mission/service/MissionService.java
+++ src/main/java/com/umc/project/mission/service/MissionService.java
@@ -1,5 +1,6 @@
 package com.umc.project.mission.service;
 
+import com.umc.project.global.page.dto.PageDTO;
 import com.umc.project.global.payload.code.status.ErrorStatus;
 import com.umc.project.mission.dto.MissionRequestDTO;
 import com.umc.project.mission.dto.MissionResponseDTO;
@@ -13,7 +14,10 @@ import com.umc.project.restaurant.entity.Restaurant;
 import com.umc.project.restaurant.service.RestaurantService;
 import com.umc.project.user.entity.User;
 import com.umc.project.user.service.UserService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -24,6 +28,7 @@ public class MissionService {
     private final MissionRepository missionRepository;
     private final UserMissionRepository userMissionRepository;
 
+    @Transactional
     public MissionResponseDTO.UserMissionCreateDTO createUserMission(MissionRequestDTO.UserMissionCreateDTO userMissionCreateDTO){
         UserMission userMission = MissionMapper.toUserMission();
 
@@ -33,21 +38,34 @@ public class MissionService {
         userMission.setUser(user);
         userMission.setMission(mission);
 
+        userMissionRepository.save(userMission);
+
         return MissionMapper.toUserMissionCreateDTO(userMission);
     }
 
+    @Transactional
     public MissionResponseDTO.MissionCreateDTO createMission(MissionRequestDTO.MissionCreateDTO missionCreateDTO){
         Mission mission = MissionMapper.toMission(missionCreateDTO);
 
         Restaurant restaurant = restaurantService.findById(missionCreateDTO.getRestaurantId());
         mission.setRestaurant(restaurant);
 
+        missionRepository.save(mission);
+
         return MissionMapper.toMissionCreateDTO(mission);
+    }
+
+    @Transactional
+    public PageDTO<MissionResponseDTO.RestaurantMissionDTO> restaurantMissions(Long restaurantId, Integer page){
+        Page<Mission> missionPage = missionRepository.findAllByRestaurantId(restaurantId, PageRequest.of(page, 10));
+
+        return MissionMapper.toRestaurantMissionPageDTO(missionPage);
     }
 
     public Mission findById(Long id){
         return missionRepository.findById(id).orElseThrow(() -> new MissionException(ErrorStatus.MISSION_NOT_FOUND));
     }
+
     public boolean existsByUserIdAndMissionId(Long userId, Long missionId){
         return userMissionRepository.existsByUser_IdAndMission_Id(userId, missionId);
     }
